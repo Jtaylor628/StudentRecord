4791de847da6d9171f065448873fbe8fecf87419
John Taylor <JTaylor01@dwc.edu>
2016-03-19 17:51:36 -0400

    commit

--- StudentRecords/StudentRecords/Source.cpp
+++ StudentRecords/StudentRecords/Source.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 #include <string>
 #include <fstream>
-struct records
+#include <vector>
+struct student_records
 {
 	string student_name;
 	int quiz1;
 	int quiz2;
 	int exam1;
 	int exam2;
-
+	int final;
+	string letter;
 
 };
 int main(){
+	
+	student_records student1;
+	student_records student2;
+	student_records student3;
+	student_records student4;
+	student_records student5;
+	student_records student6;
 
-	records student1;
+	vector <student_records> students;
+	students.push_back(student1);
+	students.push_back(student2);
+	students.push_back(student3);
+	students.push_back(student4);
+	students.push_back(student5);
+	students.push_back(student6);
 
 	ifstream in_stream;
 	ofstream out_stream;
@@ -25,6 +40,56 @@ int main(){
 
 		cout << "Input file opening failed.\n"; 
 	}
+
+	string a;
+	int count = 0;
+	int line = 0;
+	while (!in_stream.eof()) {
+		
+		in_stream >> a;
+		int begin = 0;
+		count = 0;
+		
+		for (int j = 0; j < a.size(); j++)
+		{
+				
+			if (a[j] == ',' || j == a.size() - 1)
+			{
+					
+				switch (count)
+				{
+				case 0:
+					students[line].student_name = a.substr(0, j);
+					break;
+				case 1:
+					students[line].quiz1 = stoi(a.substr(begin, j));
+					break;
+				case 2:
+					students[line].quiz2 = stoi(a.substr(begin, j));
+					break;
+				case 3:
+					students[line].exam1 = stoi(a.substr(begin, j));
+					break;
+				case 4:
+					students[line].exam2 = stoi(a.substr(begin));
+					break;
+				default:
+					break;
+				}
+				count++;
+				begin = j + 1;
+			}
+		}
+		line++;
+			
+	};
+
+	cout << student1.student_name;
+	
+
+	/*string a;
+	in_stream >> a;
+	cout << a;*/
 
 	in_stream.close();
 	out_stream.close();
